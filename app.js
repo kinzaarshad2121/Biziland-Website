a8fd6e1bb21aaf79923a40d9bb989ce87af6a94e
kinza <bsef19a515@pucit.edu.pk>
2022-08-05 17:36:47 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,8 @@
+var SideMenuBar = document.getElementById("SideMenuBar");
+function showSideMenuBar(){
+    SideMenuBar.style.right= "0%";
+    SideMenuBar.style.display= "block";
+}
+function hideSideMenuBar(){
+    SideMenuBar.style.right= "-150%";
+}
